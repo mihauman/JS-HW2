c507ecaff5319e92cea9e3b2059496403801b1d3
mihauman <mihauman1313@gmail.com>
2020-02-06 22:12:36 +0200

    JS-HW2

---
+++ main.js
@@ -0,0 +1,22 @@
+const startValue = Number(prompt('Введіть стартове число', '10'));
+const endValue = Number(prompt('Введіть кінцеве число', '100'));
+const skip = confirm('Пропускати парні числа?');
+let sum = 0;
+for(let i = startValue; i <= endValue; i++){  
+if(skip){
+        if(i%2===0)
+        {
+           continue;
+        } 
+        else
+        {
+            sum += i; 
+        }
+    }
+    else{
+        sum += i;  
+    }  
+}
+document.writeln(`<p>Start number: ${startValue}<p>`);
+document.writeln(`<p>End number: ${endValue}<p>`);
+document.writeln(`<p>Sum : ${sum}<p>`);
